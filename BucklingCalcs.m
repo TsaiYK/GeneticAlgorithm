ec619503379531cb1290ff5f29017f32330b217b
spectral01 <68571408+spectral01@users.noreply.github.com>
2021-11-08 14:47:13 -0600

    Add files via upload

--- BucklingCalcs.m
+++ BucklingCalcs.m
@@ -1,3 +1,4 @@
+function [sigma_cr_stiff, sigma_cr_skin] = BucklingCalcs(x)
 %% Calculating Moment of Inertia for stringers 
 %Constants
 E = 10.4E6; %psi
@@ -10,20 +11,20 @@ c = 4; %from definition for clamped-clamped beam
 kc = 7.2; 
 
 %Variables
-t_skin = 0.055;
-t_stiff = 0.05;
-n_stiff = 3;
-h_stiff = 5;
-w_stiff = 4;
+t_skin = x(1);
+t_stiff = x(2);
+h_stiff = x(3);
+w_stiff = x(4);
+n_stiff = x(5);
 w_domain = 60/n_stiff; 
 
 %Calculations
-L_rho_EJ = pi*sqrt((2*c*E)/sigma_y) %Value to compare to in order to determine Euler or Johnson
+L_rho_EJ = pi*sqrt((2*c*E)/sigma_y); %Value to compare to in order to determine Euler or Johnson
 %Moment of Inertia calcs
-Ixx1 = (1/12)*(w_stiff-t_stiff)*t_stiff^3 
-Iyy1 = (1/12)*t_stiff*(w_stiff-t_stiff)^3 
-Ixx2 = (1/12)*t_stiff*h_stiff^3
-Iyy2 = (1/12)*h_stiff*t_stiff^3
+Ixx1 = (1/12)*(w_stiff-t_stiff)*t_stiff^3;
+Iyy1 = (1/12)*t_stiff*(w_stiff-t_stiff)^3;
+Ixx2 = (1/12)*t_stiff*h_stiff^3;
+Iyy2 = (1/12)*h_stiff*t_stiff^3;
 A1 = (w_stiff-t_stiff)*t_stiff;
 A2 = t_stiff*h_stiff;
 Cx = ((w_stiff-t_stiff)/2*A1+(w_stiff-(t_stiff/2))*A2)/(A1+A2);
@@ -32,8 +33,8 @@ C1x = (w_stiff-t_stiff)/2;
 C1y = t_stiff/2;
 C2x = w_stiff-(t_stiff/2);
 C2y = h_stiff/2;
-Ixx_adj = (Ixx1+A1*(Cy-C1y)^2)+(Ixx2+A2*(Cy-C2y)^2)
-Iyy_adj = (Iyy1+A1*(Cx-C1x)^2)+(Iyy2+A2*(Cx-C2x)^2)
+Ixx_adj = (Ixx1+A1*(Cy-C1y)^2)+(Ixx2+A2*(Cy-C2y)^2);
+Iyy_adj = (Iyy1+A1*(Cx-C1x)^2)+(Iyy2+A2*(Cx-C2x)^2);
 
 %Additional Calcs
 A_stringer = (t_stiff*h_stiff)+(t_stiff*(w_stiff-t_stiff));
@@ -43,8 +44,11 @@ rho = min([rho_stringer_xx, rho_stringer_yy]);
 
 %Loop for 
 if L/rho > L_rho_EJ 
-    sigma_cr = c*(pi^2*E)/(L/rho)^2
+    sigma_cr_stiff = c*(pi^2*E)/(L/rho)^2;
 else 
-    sigma_cr = sigma_y*(1-((sigma_y*(L/rho)^2))/(4*c*pi^2*E))
+    sigma_cr_stiff = sigma_y*(1-((sigma_y*(L/rho)^2))/(4*c*pi^2*E));
     
 end
+
+%%Skin Buckling
+sigma_cr_skin = pi^2*kc*E/12/(1-v^2)*(t_skin/60)^2;
--- StressCal.m
+++ StressCal.m
@@ -1,12 +1,14 @@
-function [stress_skin,stress_stiff] = StressCal(x,L)
+function [stress_stiff,stress_skin] = StressCal(x)
 % x = [t_skin,t_stiff,h_stiff,w_stiff,n_stiff]
-[~,A_skin,A_stiff] = WeightCal(x,L);
+[~,A_skin,A_stiff] = WeightCal(x);
+
 
 % Forces
-w_skin = 60; % width of plate, unit: in
-F = 40*w_skin;
+w_skin = 60;
+w_domain = w_skin/x(5);% width of plate, unit: in
+F = 40*w_domain;
 F_skin = F*A_skin/(A_skin+A_stiff);
-F_stiff = F*A_stiff/(A_skin+A_stiff);
+F_stiff = F*A_stiff/x(5)/(A_skin+A_stiff);
 
 % Stresses
 stress_skin = F_skin/A_skin;
