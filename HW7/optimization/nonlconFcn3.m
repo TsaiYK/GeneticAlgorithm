91347db3cd5c6ef81fa0bd76d60737139bf55046
TsaiYK <33308568+TsaiYK@users.noreply.github.com>
2021-12-09 17:11:27 -0600

    Update nonlconFcn3.m

--- HW7/optimization/nonlconFcn3.m
+++ HW7/optimization/nonlconFcn3.m
@@ -3,7 +3,7 @@ for i = 1:3
     xDesign(i) = x(i);
 end
 xDesign(4) = 0.75;
-xDesign(5) = round(x(5));
+xDesign(5) = round(x(4));
 eigenVal = buckling_analysis(xDesign);
 c(1) = -(eigenVal/(60/xDesign(5))-40);
 ceq = [];
